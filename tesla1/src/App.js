ebea994b7a5202ee60ba530f8a7b45f3546b7765
saud-siddiqui <esouth199@gmail.com>
2021-05-30 21:16:53 +0530

    first commit

--- tesla1/src/App.js
+++ tesla1/src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>header</h1>
+      <h1>header hey saud welcome</h1>
     </div>
   );
 }
--- tesla1/src/Section.js
+++ tesla1/src/Section.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Section = () => {
     return (
         <div>
-            section
+            section hello
         </div>
     )
 }
